91909d7c2b7c7624fc23449464454de8fa1b21cd
alegian <alegiannx@gmail.com>
2026-03-19 10:35:45 +0200

    oil & snippets

---
+++ private_dot_config/nvim/lua/plugins/files.lua
@@ -0,0 +1,33 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  lazy = false,
+  keys = {
+    {
+      "-",
+      function()
+        require("oil").open()
+      end,
+      mode = "n",
+    },
+  },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    watch_for_changes = false,
+    keymaps = {
+      ["o"] = { "actions.open_external", mode = "n" },
+      ["<bs>"] = { "actions.parent", mode = "n" },
+    },
+    view_options = {
+      show_hidden = false,
+    },
+    git = {
+      mv = function(src_path, dest_path)
+        return true
+      end,
+    },
+  },
+}
